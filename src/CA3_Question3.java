8063052ef44b9b0853c8e0329a5b6390fc454ccd
D00253579 <D00253579@student.dkit.ie>
2024-01-24 16:40:47 +0000

    Question 4 finished and completing question 3
    Made a start on question 3 and have run into some issues over the last 2 days that I haven't resolved yet. Moved on to question 4 and completed it at time of commit.

--- src/CA3_Question3.java
+++ src/CA3_Question3.java
@@ -1,14 +1,38 @@
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.HashMap;
+import java.util.ArrayList;
+
 /**
- *  Name:
- *  Class Group:
+ * Name: Jamie Lawlor
+ * Class Group: SD2B
  */
 
-public class CA3_Question3
-{
-    public static void readFile(String fileName)
-    {
+public class CA3_Question3 {
+    public static void readFile(String fileName) throws FileNotFoundException {
+        File f = new File(fileName);
+        Scanner in = new Scanner(f);
 
+        in.useDelimiter("[^A-Za-z0-9_]+");
+        Map<String, Integer> Lines = new HashMap<String, Integer>();
+        String aLine = "";
+        int count = 0;
+        while (in.hasNext()) {
+            count++;
+            aLine = in.next();
+            Lines.put(aLine, count);
+        }
+
+        ArrayList<Integer> words = new ArrayList<>();
+        for (String key : Lines.keySet()) {
+            Integer value = Lines.get(key);
+            words.add(value);
+
+            System.out.println(key + " occurs at line " + words);
+
+        }
     }
 
     public static void main(String[] args) throws FileNotFoundException {
--- src/CA3_Question4.java
+++ src/CA3_Question4.java
@@ -2,18 +2,41 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.util.Stack;
+
 /**
- *  Name:
- *  Class Group:
+ * Name: Jamie Lawlor
+ * Class Group: SD2B
  */
 public class CA3_Question4 {
 
     /*
         filename: name of the file to test.
      */
-    public static boolean validate(String filename) throws FileNotFoundException
-    {
-        return false;
+    public static boolean validate(String filename) throws FileNotFoundException {
+        File f = new File(filename);
+        Scanner key = new Scanner(f);
+        String aLine;
+        Stack<String> tags = new Stack<>();
+        while (key.hasNext()) {
+            aLine = key.next();
+            //Pushes the starting tags onto the stack
+            if (!aLine.contains("</")) {
+                tags.push(aLine);
+
+            }
+            //Comparing the end tags in the stack to the starting tags and pops the end stack when they match
+            if (aLine.contains("</")) {
+                /*Creates a substring of the ending tags with its starting point being after the "/"
+                and checks does it equal the substring of the starting tags after the "<" in order.
+                * */
+                if (aLine.substring(2).equals(tags.peek().substring(1))) {
+                    tags.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
     /*
@@ -26,8 +49,8 @@ public class CA3_Question4 {
      */
     public static void main(String[] args) throws FileNotFoundException {
         String[] files = {"tags_valid.txt", "tags_invalid.txt"};
-        for(String fName: files) {
-            System.out.print(fName +": ");
+        for (String fName : files) {
+            System.out.print(fName + ": ");
             if (validate(fName)) {
                 System.out.println("This file is valid");
             } else {
@@ -36,3 +59,4 @@ public class CA3_Question4 {
         }
     }
 }
+
